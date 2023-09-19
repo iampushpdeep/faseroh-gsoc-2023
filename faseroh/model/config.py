64a73c55aca2dda64708c2d577eca57bbb9ba26f
iampushpdeep <pushpdeep30.com>
2023-09-19 18:15:29 +0530

    model

---
+++ faseroh/model/config.py
@@ -0,0 +1,81 @@
+from dataclasses import dataclass
+from typing import List, Optional, Union
+
+try:
+    from typing import Literal
+except ImportError:
+    from typing_extensions import Literal
+
+@dataclass
+class PositionalConfig:
+    max_len: int
+    d_model: int
+    vocab_size: int
+
+@dataclass
+class EncoderConfig:
+    dff: int
+    num_heads: int
+    num_layers: int
+    d_model: int
+    dropout_rate: Optional[float] = None
+    vocab_size: int
+
+@dataclass
+class DecoderConfig:
+    d_model: int
+    num_heads: int
+    d_ff: int
+    num_layers: int
+    max_len: int
+    dropout_rate: float = 0.1
+
+@dataclass
+class DecoderWithRegressionConfig(DecoderConfig):
+    mode: DecoderMode = "sum"
+
+@dataclass
+class DatasetConfig:
+    path: str
+    valid_path: str
+    num_of_not_seen: int
+    batch_size: int
+    sample_points: bool
+    test_size: int
+    dataset_size: int
+    variables: List[str]
+    num_points: int
+    extended_representation: bool = True
+
+
+@dataclass
+class GreedySearchConfig:
+    max_num: int
+    evaluate_each: int
+
+
+@dataclass
+class BeamSearchConfig(GreedySearchConfig):
+    beam_width: int
+
+
+@dataclass
+class CallbackConfig:
+    greedy_search: GreedySearchConfig
+    beam_search: BeamSearchConfig
+    regression_delay: int
+
+
+@dataclass
+class Config:
+    encoder_config: EncoderConfig
+    decoder_config: Union[DecoderConfig, DecoderWithRegressionConfig]
+    loss: Literal["mse", "mae", "huber"]
+    reg_head_num_layers: int
+    reg_head_dim: int
+    dataset_config: DatasetConfig
+    max_epoch: int
+    input_regularizer: Optional[Regularizer]
+    callback_config: CallbackConfig
+    regression_lambda: float = 0.01
+    label_smoothing: float = 0.0
